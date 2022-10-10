fed5a761f2741930a28fe55a457278fd221ad5bd
yekkhansiang <b160003@e.ntu.edu.sg>
2022-10-10 22:21:51 +0800

    first commit

---
+++ drello/src/main/java/com/example/drello/DrelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.drello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrelloApplication.class, args);
+    }
+
+}
---
+++ drello/src/test/java/com/example/drello/DrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
